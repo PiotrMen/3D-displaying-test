21286368e12ae8e690e66669907f68233fddb700
PiotrMen <scaryaxpl@gmail.com>
2024-06-07 20:02:41 +0200

    vxproj updated

--- 3D displaying test/3D displaying test/Main.cpp
+++ 3D displaying test/3D displaying test/Main.cpp
@@ -1,11 +1,11 @@
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "GLFW/glfw3.h"
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
