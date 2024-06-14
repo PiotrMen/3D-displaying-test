79ec757ec9164cafa0991e33794b97e8673bdc7e
PiotrMen <scaryaxpl@gmail.com>
2024-06-14 12:07:24 +0200

    cleaning unused code

--- 3D displaying test/3D displaying test/Main.cpp
+++ 3D displaying test/3D displaying test/Main.cpp
@@ -127,18 +127,9 @@ int main()
 		projection = glm::perspective(glm::radians(45.0f), static_cast<float>(mode->width) / static_cast<float>(mode->height), 0.1f, 1000.0f);
 
 
-		//unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-		//unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
-		//unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
-
-		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		//glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		//glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
 		shaderProgram.setMat4("model", model);
 		shaderProgram.setMat4("view", view);
 		shaderProgram.setMat4("projection", projection);
-		//setModelViewProjection(shaderProgram.getID(), model, view, projection);
 
 		Model.bind();
 		glDrawArrays(GL_TRIANGLES, 0, vertices.size() / 3);
