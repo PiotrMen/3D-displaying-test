7d661fad8318328f079723dfc012959309313d4c
PiotrMen <scaryaxpl@gmail.com>
2024-06-14 16:07:01 +0200

    shader updated

--- 3D displaying test/3D displaying test/shaders/ligthing_fragment_shader.glsl
+++ 3D displaying test/3D displaying test/shaders/ligthing_fragment_shader.glsl
@@ -8,11 +8,11 @@ uniform vec3 lightPos;
 uniform vec3 viewPos;
 uniform vec3 lightColor;
 uniform vec3 objectColor;
-
+uniform float ambientStrength;
+uniform float specularStrength;
 void main()
 {
     // ambient
-    float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
   	
     // diffuse 
@@ -22,7 +22,6 @@ void main()
     vec3 diffuse = diff * lightColor;
     
     // specular
-    float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
